be64e83cc374e9d635a25c42fd66300f8622acb5
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-01-21 22:11:25 +0530

    Create 21 Jan: 198 House Robber.cpp

---
+++ 01 January/21 Jan: 198 House Robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int rob = 0;
+        int norob = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int newRob = norob + nums[i];
+            int newNoRob = max(norob, rob);
+            rob = newRob;
+            norob = newNoRob;
+        }
+        return max(rob, norob);
+    }
+};
