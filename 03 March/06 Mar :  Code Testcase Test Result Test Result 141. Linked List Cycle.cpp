e021f8df2b9593840c9923e5bba55a9b0b903901
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-03-06 20:09:36 +0530

    Create 06 Mar :  Code Testcase Test Result Test Result 141. Linked List Cycle.cpp

---
+++ 03 March/06 Mar :  Code Testcase Test Result Test Result 141. Linked List Cycle.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head , *slow = head;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast == slow) return true;
+        }
+        return false;
+    }
+};
