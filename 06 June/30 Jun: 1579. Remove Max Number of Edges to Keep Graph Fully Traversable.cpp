3dea764731bbfc6d779d95fa01c23b7cbd909b9c
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-06-30 19:52:40 +0530

    Create 30 Jun: 1579. Remove Max Number of Edges to Keep Graph Fully Traversable.cpp

---
+++ 06 June/30 Jun: 1579. Remove Max Number of Edges to Keep Graph Fully Traversable.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        class UnionFind {
+        public:
+            vector<int> parent, size;
+            int components;
+            UnionFind(int n) {
+                components = n;
+                parent.resize(n + 1);
+                size.resize(n + 1, 1);
+                for (int i = 0; i <= n; ++i) {
+                    parent[i] = i;
+                }
+            }
+
+            int find(int x) {
+                if (parent[x] != x) {
+                    parent[x] = find(parent[x]);
+                }
+                return parent[x];
+            }
+
+            bool unite(int x, int y) {
+                int rootX = find(x), rootY = find(y);
+                if (rootX == rootY) return false;
+                if (size[rootX] < size[rootY]) swap(rootX, rootY);
+                parent[rootY] = rootX;
+                size[rootX] += size[rootY];
+                components--;
+                return true;
+            }
+
+            bool isConnected() {
+                return components == 1;
+            }
+        };
+        
+        UnionFind alice(n), bob(n);
+        int edgesRequired = 0;
+        
+        for (const auto& edge : edges) {
+            if (edge[0] == 3) {
+                if (alice.unite(edge[1], edge[2]) | bob.unite(edge[1], edge[2])) {
+                    edgesRequired++;
+                }
+            }
+        }
+       
+        for (const auto& edge : edges) {
+            if (edge[0] == 1) {
+                if (alice.unite(edge[1], edge[2])) {
+                    edgesRequired++;
+                }
+            } else if (edge[0] == 2) {
+                if (bob.unite(edge[1], edge[2])) {
+                    edgesRequired++;
+                }
+            }
+        }
+        
+        if (alice.isConnected() && bob.isConnected()) {
+            return edges.size() - edgesRequired;
+        }
+        
+        return -1;
+    }
+};
