472ad77a89c5669999232b638228ac6803fc15de
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-07-12 20:34:34 +0530

    Create 12 Jul; 1717. Maximum Score From Removing Substrings.cpp

---
+++ 07 July/12 Jul; 1717. Maximum Score From Removing Substrings.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+         int res = 0;
+        string top, bot;
+        int top_score, bot_score;
+
+        if (y > x) {
+            top = "ba";
+            top_score = y;
+            bot = "ab";
+            bot_score = x;
+        } else {
+            top = "ab";
+            top_score = x;
+            bot = "ba";
+            bot_score = y;
+        }
+
+        vector<char> stack;
+        for (char ch : s) {  // Changed 'char' to 'ch'
+            if (ch == top[1] && !stack.empty() && stack.back() == top[0]) {
+                res += top_score;
+                stack.pop_back();
+            } else {
+                stack.push_back(ch);
+            }
+        }
+
+        vector<char> new_stack;
+        for (char ch : stack) {  
+            if (ch == bot[1] && !new_stack.empty() && new_stack.back() == bot[0]) {
+                res += bot_score;
+                new_stack.pop_back();
+            } else {
+                new_stack.push_back(ch);
+            }
+        }
+
+        return res;
+    }
+};
