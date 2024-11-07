1f6711d8638ef9eacb54207d7e7de4108c77ba61
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-11-07 20:57:45 +0530

    Create 07 Nov: 2275. Largest Combination With Bitwise AND Greater Than Zero.cpp

---
+++ 11 November/07 Nov: 2275. Largest Combination With Bitwise AND Greater Than Zero.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int n = candidates.size(),ans = 0;
+        for(int i=0;i<32;i++){
+            int cnt = 0;
+            for(auto candidate : candidates){
+                if(candidate & (1<<i))cnt++;
+            }
+            ans = max(ans,cnt);
+        }
+        return ans;
+    }
+};
