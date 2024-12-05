05e4b22d6b8f44b3c994a291a87a415c7074d42e
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-12-05 19:54:02 +0530

    Create 05 Dec: 2337. Move Pieces to Obtain a String.cpp

---
+++ 12 December/05 Dec: 2337. Move Pieces to Obtain a String.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canChange(string start, string target) {
+        int n=s.size();
+        for(int i=0, j=0; i<n || j<n; i++, j++){
+            while(i<n && s[i]=='_') i++;
+            while(j<n && t[j]=='_') j++;
+            char c=s[i];
+            if (c !=t[j]) return 0;
+            if (c =='L' && i<j) return 0;
+            if (c =='R' && i>j) return 0;
+        }
+        return 1;
+    }
+};
