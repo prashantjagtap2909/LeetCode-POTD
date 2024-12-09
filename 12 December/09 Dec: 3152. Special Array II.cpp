c9de03bb3addcfc5f4881f5c21dd08f3d51bd71f
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2024-12-09 21:49:03 +0530

    Create 09 Dec: 3152. Special Array II.cpp

---
+++ 12 December/09 Dec: 3152. Special Array II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<int> prefix(n, 0);
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1];
+            if ((nums[i - 1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i - 1] % 2 != 0 && nums[i] % 2 != 0)) {
+                prefix[i]++;
+            }
+        }
+        vector<bool> ans;
+        for (auto& q : queries) {
+            int left = q[0], right = q[1];
+            int specialCount = prefix[right] - (left > 0 ? prefix[left] : 0);
+            ans.push_back(specialCount == 0);
+        }
+        return ans;
+    }
+};
